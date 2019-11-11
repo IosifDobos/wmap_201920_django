575be77e64030c6817240a97360f4bc3f746da8d
Mark Foley <mark@markfoley.info>
2019-11-11 16:21:27 +0000

    Adjusted deployment options

--- wmap_201920_django/settings.py
+++ wmap_201920_django/settings.py
@@ -16,6 +16,7 @@ from django.urls import path, include, reverse_lazy, reverse
 from . import secrets
 SECRETS = secrets.get_secrets()
 secrets.insert_domainname_in_conf(SECRETS["NGINX_CONF"], SECRETS["MY_DOMAIN_NAME"])
+secrets.insert_imagename_in_compose(SECRETS["DOCKER_COMPOSE_FILE"], SECRETS["DOCKER_IMAGE"])
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
